be2de390230765414d7e9ac3f70a6dae6bcf6628
paulina.arregocesg@udea.edu.co <paulina.arregocesg@udea.edu.co>
2025-10-01 22:03:24 -0300

    Cambios conflicto

--- PruebaQtg5/main.cpp
+++ PruebaQtg5/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 
     cout <<"Prueba de cambios en Qt";
-    cout <<"Cambio por colaborador1";
+    cout <<"Cambio por colaborador1 que causa conflicto";
     cout <<"Cambios en la rama verde";
 
     return 0;
